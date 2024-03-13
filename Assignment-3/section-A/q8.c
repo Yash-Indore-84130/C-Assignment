97679128114e77bd76f325188aa770e72b8445dd
DESD-Yash_Indore-84130 <indoreyash2001@gmail.com>
2024-03-13 21:25:55 +0530

    Assignment-3section-B

--- Assignment-3/section-A/q8.c
+++ Assignment-3/section-A/q8.c
@@ -1,54 +1,16 @@
 #include <stdio.h>
 
-int euclidean_gcd(int a, int b) {
-    int temp;
-    while (b != 0) {
-        temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-int main() {
-    int a, b;
-    printf("Enter the first number: ");
-    scanf("%d", &a);
-    printf("Enter the second number: ");
-    scanf("%d", &b);
-
-    
-    int gcd = euclidean_gcd(a, b);
-    printf("The GCD of %d and %d is %d\n", a, b, gcd);
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*#include<stdio.h>
 int main()
 {
 int a,b;
 printf("Enter two number:");
 scanf("%d %d",&a,&b);
-while(b!=0)
-{
-int temp=b;
-b=a%b;
-a=b;
-
-}
+int temp;
+ while (b != 0) {
+    temp = b;
+    b = a % b;
+    a = temp;
+    }
 printf("%d\n",a);
 
-}*/
+}
